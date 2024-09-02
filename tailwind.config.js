b9806364ad1365f17bbf77fe8763e68dc5703ca8
10blowblow1921 <colitajohnpaul2@gmail.com>
2024-09-03 00:04:56 +0800

    update the homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./view/*.html"],
+  content: ["./view/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: { 
+      boxShadow: {
+        'custom': '0px 4px 4px 0px #00000040',
+      }
+     },
+    screens: {
+      'phone': '375px',
+      // => @media (min-width: 375px) { ... }
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
